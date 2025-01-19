e7eb3467652792d81804e7b468ca06ccbfebba8b
Kashfi Mehbuba <kashfimehbuba71@gmail.com>
2025-01-19 01:07:16 -0500

    Lab 1 Exercise Adding

---
+++ Week01/lab1.py
@@ -0,0 +1,26 @@
+# Sample Coding Questions 01 Week 01
+# First Name: Kashfi, Last Name: Mehbuba
+
+# Defining Variables: 
+my_array = [1, 4, 7, 9]
+
+# Order of Operations
+a = 1
+b = 2
+c = 3
+d = 4
+
+# Fully bracketed version of e = a * c - b / d
+e1 = (a * c) - (b / d)
+
+# Fully bracketed version of e = a - b ** c // d + a % c
+e2 = ((a - ((b ** c) // d)) + (a % c))
+
+# Formatting
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+# Common Functions
+userAge = int(input("Enter your age: "))
+age_adjusted = userAge + 22
+print("Now showing the shop items filtered by age:", age_adjusted)
